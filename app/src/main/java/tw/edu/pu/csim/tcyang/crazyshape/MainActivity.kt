28da716ca10aaba0aed441a0ab444585f267b82e
puikefarn <wochaoniba1234@gmail.com>
2021-05-31 10:52:37 +0800

    2021上机考v2_貝克帆

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -14,6 +14,9 @@ public final class MyAppGlideModule : AppGlideModule()
 
 
 class MainActivity : AppCompatActivity() {
+
+    var Flag:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity() {
                 .override(800, 600)
                 .into(imgTitle)
 
-
+        RndShape()
 
 
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
@@ -34,5 +37,26 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+
+
+        imgNext.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?){
+            RndShape()
+            }
+        })
     }
+
+    fun RndShape(){
+        Flag=(1..4).random()
+        when(Flag){
+            1->imgNext.setImageResource(R.drawable.circle)
+            2->imgNext.setImageResource(R.drawable.square)
+            3->imgNext.setImageResource(R.drawable.star)
+            4->imgNext.setImageResource(R.drawable.triangle)
+        }
+
+
+    }
+
+
 }
